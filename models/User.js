a1165dc667801b1c556c8e797831bd8e3eac148c
EdisonJuniorDesigner <mage1232010@gmail.com>
2020-07-17 00:59:59 -0300

    initial Commit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+class User {
+
+    constructor(name, email, password, gender, birth, country, photo, admin) {
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.birth = birth;
+        this.country = country;
+        this.photo = photo;
+        this.admin = admin;
+
+    }
+
+}
