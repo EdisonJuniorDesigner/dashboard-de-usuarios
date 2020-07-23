0266491686ca4a5a3cc9ffad3771663b1cdf6cbe
EdisonJuniorDesigner <mage1232010@gmail.com>
2020-07-22 21:24:17 -0300

    Edit View User, Count Users and Admins

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,17 @@ class UserController {
     this.tableEl = document.getElementById(tableId);
 
     this.onSubmit();
+    this.onEdit();
+  }
+
+  onEdit() {
+
+    document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+
+      this.showPanelCreate();
+
+    });
+
   }
 
   onSubmit() {
@@ -92,19 +103,21 @@ class UserController {
     }
 
     return new User(
-      user.name,
-      user.gender,
-      user.birth,
-      user.email,
-      user.country,
-      user.password,
-      user.photo,
+      user.name, 
+      user.gender, 
+      user.birth, 
+      user.country, 
+      user.email, 
+      user.password, 
+      user.photo, 
       user.admin
-    );
+  );
   }
 
   addLine(dataUser) {
     let tr = document.createElement("tr");
+
+    tr.dataset.user = JSON.stringify(dataUser);
 
     tr.innerHTML = `
             <tr>
@@ -116,12 +129,74 @@ class UserController {
                 <td>${dataUser.admin ? "Sim" : "Não"}</td>
                 <td>${Utils.dateformat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
 
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+          let json = JSON.parse(tr.dataset.user);
+          let form = document.querySelector("#form-user-update");
+          
+          for (let name in json) {
+
+            let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+            console.log(json);
+
+            if (field){
+              
+              if (field.type == "file") continue;
+              
+              field.value = json[name];
+              
+            }
+
+          }
+          
+          this.showPanelUpdate();
+
+        });
+
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+
+  }
+
+  showPanelCreate(){
+
+    document.querySelector("#box-user-create").style.display = "block";
+    document.querySelector("#box-user-update").style.display = "none";
+
+  }
+
+  showPanelUpdate(){
+
+    document.querySelector("#box-user-create").style.display = "none";
+    document.querySelector("#box-user-update").style.display = "block";
+
+  }
+
+  updateCount() {
+
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr=>{
+
+      numberUsers++;
+      
+      let user = JSON.parse(tr.dataset.user);
+
+      if (user._admin) numberAdmin++;
+
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
   }
 }
--- models/User.js
+++ models/User.js
@@ -1,53 +1,71 @@
 class User {
-  constructor(name, email, password, gender, birth, country, photo, admin) {
-    this._name = name;
-    this._email = email;
-    this._password = password;
-    this._gender = gender;
-    this._birth = birth;
-    this._country = country;
-    this._photo = photo;
-    this._admin = admin;
-    this._register = new Date();
+
+  constructor(name, gender, birth, country, email, password, photo, admin){
+
+      this._id;
+      this._name = name;
+      this._gender = gender;
+      this._birth = birth;
+      this._country = country;
+      this._email = email;
+      this._password = password;
+      this._photo = photo;
+      this._admin = admin;
+      this._register = new Date();
+
   }
 
-  get register() {
-    return this._register;
+  get id(){
+      return this._id;
   }
+
+  get register(){
+      return this._register;
+
+  }
+
 
   get name() {
-    return this._name;
-  }
+      return this._name;
 
-  get email() {
-    return this._email;
-  }
-
-  get password() {
-    return this._password;
   }
 
   get gender() {
-    return this._gender;
+      return this._gender;
+
   }
 
   get birth() {
-    return this._birth;
+      return this._birth;
+
   }
 
   get country() {
-    return this._country;
+      return this._country;
+
+  }
+
+  get email() {
+      return this._email;
+
   }
 
   get photo() {
-    return this._photo;
+      return this._photo;
+
+  }
+
+  get password(){
+      return this._password;
+
   }
 
   get admin() {
-    return this._admin;
+      return this._admin;
   }
 
-  set photo(value) {
-    this._photo = value;
+  set photo(value){
+      this._photo = value;
+      
   }
 }
